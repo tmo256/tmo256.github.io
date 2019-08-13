91622faf187be2f57ca472fc244029fe6dd28700
marlon-ul <53267564+marlon-ul@users.noreply.github.com>
2019-08-13 13:16:56 -0500

    TOC js file
    This js file inserts the TOC into the html files

---
+++ assets/js/toc.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    var ToC =
+        "<nav role='navigation' class='example toc'>" +
+        "<h4 class='ul-ui header'>Common UI/UX</h4>" +
+        "<div class='ul-ui middle aligned divided list'>" +
+        "<div class='item'>" +
+        "<div class='content'>" +
+        "<div class='header'>Introduction</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "<div class='item'>" +
+        "<div class='content'>" +
+        "<div class='header'>Common Foundation</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "<div class='item'>" +
+        "<div class='content'>" +
+        "<div class='header'>Style Guide</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "<div class='item'>" +
+        "<div class='content'>" +
+        "<div class='header'>Component Library</a>" +
+        "</div>" +
+        "<a id='typography' class='item block' href='typography.html'>Typography</a>" +
+        "<a id='alerts' class='item block' href='alerts.html'>Alerts</a>" +
+        "<a id='buttons' class='item block' href='buttons.html'>Anchors &amp; Buttons</a>" +
+        "</div>" +
+        "</div>" +
+        "</div>" +
+        "</nav>";
+    $("article").prepend(ToC);
+});
