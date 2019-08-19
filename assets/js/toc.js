9cfc6ee193073b31bbd5c31a91b25d82e1574952
Marlon <marlon.dequevedo@ul.com>
2019-08-19 15:38:27 -0500

    no message

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -25,11 +25,11 @@ $(document).ready(function () {
         "<div class='content'>" +
         "<div class='header'>Component Library</a>" +
         "</div>" +
+        //"<a id='typography' class='item block' href='typography.html'>Typography</a>" +
+        //"<a id='alerts' class='item block' href='alerts.html'>Alerts</a>" +
         "<a id='buttons' class='item block' href='buttons.html'>Anchors &amp; Buttons</a>" +
-        "<a id='typography' class='item block' href='typography.html'>Typography</a>" +
-        "<a id='alerts' class='item block' href='alerts.html'>Alerts</a>" +
-        "<a id='lists' class='item block' href='lists.html'>Lists</a>" +
-        "<a id='menu' class='item block' href='menu.html'>Menus</a>" +
+        //"<a id='lists' class='item block' href='lists.html'>Lists</a>" +
+        //"<a id='menu' class='item block' href='menu.html'>Menus</a>" +
         "</div>" +
         "</div>" +
         "</div>" +
